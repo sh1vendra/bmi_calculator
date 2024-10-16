828b93739e0c4b337fae443022f82a73532c1355
sh1vendra <shivendra@txstate.edu>
2024-10-15 21:54:04 -0500

    Add weight input slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
+    <main>
+      <h1>BMI CALCULATOR</h1>
+      <div className=" input-section ">
+        <p className="slider-output"> Weight: 80 kg </p>
+        <input
+          className="input-slider"
+          type="range"
+          step="1"
+          min="40"
+          max="200"
+        />
+      </div>
+    </main>
+  );
+};
 
-      
-    </div>
-  )
-}
-
-export default App
+export default App;
