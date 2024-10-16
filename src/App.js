d7d560fd16e6cc0108550246574a8c44809f2379
sh1vendra <shivendra@txstate.edu>
2024-10-15 22:11:21 -0500

    Add state management for weight and height sliders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
 
 const App = () => {
+  const [hieght, setHeight] = useState(70);
+  const [wieght, setWeight] = useState(180);
+
+  function onWeightChange(event){
+
+  }
+  function onHeightChange(event){
+    
+  }
   return (
     <main>
       <h1>BMI CALCULATOR</h1>
       <div className=" input-section ">
-        <p className="slider-output"> Weight: 80 kg </p>
+        <p className="slider-output"> Weight: {weight} kg </p>
         <input
           className="input-slider"
           type="range"
           step="1"
           min="40"
-          max="200"  />
-          <p className ='slider-output'> Height: 180 cm</p>
-          <input 
-          className= 'input-slider'
-          type='range' />
-          
+          max="200"
+          onChange={onWeightChange}
 
-       
+        />
+        <p className="slider-output"> Height: {height} cm</p>
+        <input className="input-slider" type="range"
+         onChange={onHeightChange} />
+      </div>
+      <div className="output-section">
+        <p>Your BMI Index is</p>
+        <p className="output">100</p>
       </div>
     </main>
   );
